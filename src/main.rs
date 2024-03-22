5fb67e8e4c66e03bda882a543e5c3f1c544429f0
Martin Contreras <martin.contreras.u@outlook.com>
2024-03-22 12:01:41 -0300

    WIP: reading csv

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![allow(unused)]
 use anyhow::{Error as E, Result};
 use clap::{Parser, Subcommand, ValueEnum};
+use polars::prelude::*;
+use polars_core::prelude::*;
+use polars_io::prelude::*;
 
 #[derive(Clone, Copy, ValueEnum, Debug)]
 enum Format {
@@ -25,5 +28,12 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    println!("{:?}", args.path);
+    let lf1: PolarsResult<DataFrame> = {
+        CsvReader::from_path(args.path)
+            .unwrap()
+            .has_header(true)
+            .finish()
+    };
+
+    println!("{:?}", lf1.unwrap().head(Some(5)));
 }
---
+++ tests/cli.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+#[test]
+fn dies_no_args() -> Result<()> {
+    Command::cargo_bin("dscribe")?
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("Usage"));
+    Ok(())
+}
+#[test]
+fn accept_path() -> Result<()> {
+    Command::cargo_bin("dscribe")?
+        .args(&["data_examples/onlinefood.csv"])
+        .assert()
+        .success();
+    Ok(())
+}
