fb332ab93b4ce2cdb8ae0ac513fe6995ba209f99
Martin Contreras <martin.contreras.u@outlook.com>
2024-04-09 14:05:02 -0400

    fixing test

--- tests/cli.rs
+++ tests/cli.rs
@@ -13,7 +13,7 @@ fn dies_no_args() -> Result<()> {
 #[test]
 fn accept_path() -> Result<()> {
     Command::cargo_bin("dscribe")?
-        .args(&["data_examples/onlinefoods.csv"])
+        .args(&["data_examples/onlinefoods.csv", "head"])
         .assert()
         .success();
     Ok(())
