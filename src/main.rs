42aec60fb473275d3d2690b270da95233e1a64e6
Martin Contreras <martin.contreras.u@outlook.com>
2024-04-09 17:36:39 -0400

    SQL, head, show and summary applied

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![allow(unused)]
+use std::fmt::Debug;
+use std::sync::Arc;
+
 #[macro_use]
 use std::path::PathBuf;
+use arrow::datatypes::{DataType, Field, Schema};
 use clap::{Parser, Subcommand, ValueEnum};
 use datafusion::datasource::file_format::file_compression_type::FileCompressionType;
 use datafusion::error::Result;
@@ -19,10 +23,14 @@ enum Format {
 enum Operation {
     /// show top 5 rows
     Head,
+    /// show N top rows
+    Show { n: usize },
+    /// sample N rows
+    Sample,
     /// show the summary of every column
     Summary,
-    /// Use SQL to query the file
-    Query { query: String },
+    /// Use SQL to query the file, the table name is 'input' by default
+    Sql { query: String },
     /// get columns names
     Columns,
 }
@@ -56,12 +64,49 @@ async fn main() -> Result<()> {
     let args = Args::parse();
     println!("{:?}", args);
 
+    let schema = arrow_csv::reader::infer_schema_from_files(
+        &[args.path.to_str().unwrap().to_string()],
+        args.delimiter.as_bytes()[0],
+        Some(1000),
+        args.header,
+    )?;
+
+    let mut fields = schema
+        .fields()
+        .iter()
+        .map(|field| {
+            let field_name = field.name().to_ascii_lowercase().replace(' ', "_");
+            let datatype = field.data_type();
+            let nullable = field.is_nullable();
+            Field::new(field_name, datatype.clone(), nullable)
+        })
+        .collect::<Vec<Field>>();
+
+    let schema = Arc::new(Schema::new(fields));
+
     let ctx = SessionContext::new();
     let options = CsvReadOptions::new()
         .delimiter(args.delimiter.as_bytes()[0])
-        .has_header(args.header);
+        .has_header(args.header)
+        .schema(&schema);
 
-    let df = ctx.read_csv(args.path.to_str().unwrap(), options).await?;
+    // register csv file with the execution context
+    ctx.register_csv("input", args.path.to_str().unwrap(), options)
+        .await?;
+
+    let df = ctx.table("input").await?;
+
+    let df = match args.operation {
+        Operation::Head => df.limit(0, Some(5))?,
+        Operation::Show { n: val } => df.limit(0, Some(val))?,
+        Operation::Summary => df.describe().await.unwrap(),
+        Operation::Sql { query } => ctx.sql(&query).await?,
+        Operation::Columns => {
+            println!("{}", df.schema());
+            todo!()
+        }
+        _ => todo!(),
+    };
 
     let result = df.collect().await?;
 
